294d9ed01eb37f20ac76c68c21e40144af945649
Christopher Hsu <70501069+christopherchsu@users.noreply.github.com>
2021-10-28 10:18:47 -0500

    Time: 96 ms (75.02%), Space: 48.9 MB (40.88%) - LeetHub

--- lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.js
+++ lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.js
@@ -14,29 +14,13 @@
  */
 var lowestCommonAncestor = function(root, p, q) {
     while ((root.val >= p.val && root.val >= q.val) || (root.val <= q.val && root.val <= p.val)) {
-        var parent = root;
         if (root.val === p.val || root.val === q.val) {
             return root;
         } else if (root.val < p.val && root.val < q.val) {
             root = root.right;
         } else if (root.val > p.val && root.val > q.val) {
             root = root.left;
-        } else {
-            return parent;
         }
     }
     return root;
-    
 };
-
-
-// var isDescendent = function(root, node) {
-//     if (root === null) {
-//         return false;
-//     }
-//     if (root.val === node.val) {
-//         return true;
-//     } else {
-//         return isDescendent(root.right, node) || isDescendent(root.left, node);
-//     }
-// }
