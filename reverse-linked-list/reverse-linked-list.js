96c5254df4266942f7c28b699ac356df9c6b6497
Christopher Hsu <70501069+christopherchsu@users.noreply.github.com>
2021-10-30 18:26:20 -0500

    Time: 80 ms (68.49%), Space: 40.5 MB (61.72%) - LeetHub

---
+++ reverse-linked-list/reverse-linked-list.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+
+var reverseList = function(head) {
+    var reverse = null;
+    var reversePrev = null;
+    while (head) {
+        reverse = head;
+        head = head.next;
+        reverse.next = reversePrev
+        reversePrev = reverse;
+    }
+    return reverse;
+
+};
