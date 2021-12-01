bccfe44507ac5599cabe446d0ac276ea9920e943
Christopher Hsu <70501069+christopherchsu@users.noreply.github.com>
2021-12-01 15:51:44 -0600

    Time: 100 ms (57.38%), Space: 41 MB (77.05%) - LeetHub

---
+++ set-matrix-zeroes/set-matrix-zeroes.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var setZeroes = function(matrix) {
+    // var zeroIndices = [];
+    // for (var i = 0; i < matrix.length; i++) {
+    //     for (var j = 0; j < matrix[i].length; j++) {
+    //         if (matrix[i][j] === 0) {
+    //             zeroIndices.push([i, j]);
+    //         }
+    //     }
+    // }
+    
+    var setVisited = function(row, col) {
+        for (var i = 0; i < matrix.length; i++) {
+            if (matrix[i][col] !== 0) {
+                matrix[i][col] = 'visited'
+            }
+        }
+        for (var i = 0; i < matrix[0].length; i++) {
+            if (matrix[row][i] !== 0) {
+                matrix[row][i] = 'visited'
+            }
+        }
+    }
+    
+    for (var row = 0; row < matrix.length; row++) {
+        for (var col = 0; col < matrix[0].length; col++) {
+            if (matrix[row][col] === 0) {
+                setVisited(row, col);
+            }
+        }
+    }
+    for (var row = 0; row < matrix.length; row++) {
+        for (var col = 0; col < matrix[0].length; col++) {
+            if (matrix[row][col] === 'visited') {
+                matrix[row][col] = 0;
+            }
+        }
+    }
+    return matrix;
+};
