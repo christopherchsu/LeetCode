d96507e43c12955e8afedadc48ea43d87b5a9e4c
Christopher Hsu <70501069+christopherchsu@users.noreply.github.com>
2021-12-21 15:09:02 -0600

    Time: 84 ms (75.09%), Space: 42.4 MB (69.40%) - LeetHub

--- employee-importance/employee-importance.js
+++ employee-importance/employee-importance.js
@@ -14,27 +14,22 @@
  */
 var GetImportance = function(employees, id) {
     var result = 0;
+    var hashMap = {};
+    for (var i = 0; i < employees.length; i++) {
+        hashMap[employees[i].id] = employees[i];
+    } 
     var recurse = function(employee) {
-        console.log(employee);
         if (employee === null) {
             return;
         } else {
             result += employee.importance
             if (employee.subordinates) {
                 for (var i = 0; i < employee.subordinates.length; i++) {
-                    for (var j = 0; j < employees.length; j++) {
-                        if (employees[j].id === employee.subordinates[i]) {
-                            recurse(employees[j]);
-                        }
-                    }
+                    recurse(hashMap[employee.subordinates[i]]);
                 }   
             }
         }
     }
-    for (var i = 0; i < employees.length; i++) {
-        if (employees[i].id === id) {
-            recurse(employees[i]);
-        }
-    }
+    recurse(hashMap[id]);
     return result;
 };
