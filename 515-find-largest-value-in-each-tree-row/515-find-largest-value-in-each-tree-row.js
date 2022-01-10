52d7b7142dcedd432e905ac926e045ff2a6cefe5
Christopher Hsu <70501069+christopherchsu@users.noreply.github.com>
2022-01-10 13:23:31 -0600

    Time: 196 ms (5.37%), Space: 44.2 MB (19.01%) - LeetHub

---
+++ 515-find-largest-value-in-each-tree-row/515-find-largest-value-in-each-tree-row.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var largestValues = function(root) {
+    if (root === null) {
+        return [];
+    }
+    var hashMap = {};
+    var queue = [[0, root]];
+    while (queue.length !== 0) {
+        var node = queue.shift();
+        if (node[1].left) {
+            queue.push([node[0] + 1, node[1].left]);
+        }
+        if (node[1].right) {
+            queue.push([node[0] + 1, node[1].right]);
+        }
+        if (hashMap[node[0]] === undefined) {
+            hashMap[node[0]] = node[1].val;
+        } else {
+            if (hashMap[node[0]] < node[1].val) {
+                hashMap[node[0]] = node[1].val;
+            }
+        }
+    }
+    var result = [];
+    for (var key in hashMap) {
+        result.push(hashMap[key]);
+    }
+    return result;
+};
