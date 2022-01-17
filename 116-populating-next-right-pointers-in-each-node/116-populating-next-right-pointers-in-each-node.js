f7955735ee9d022288f1222f3473d92b32521702
Christopher Hsu <70501069+christopherchsu@users.noreply.github.com>
2022-01-17 00:14:59 -0600

    Time: 131 ms (33.13%), Space: 45.6 MB (30.68%) - LeetHub

---
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.js
@@ -0,0 +1,36 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+
+var connect = function(root) {
+    if (root === null) {
+        return root;
+    }
+    var queue = [[root, 0]];
+    while (queue.length !== 0) {
+        cur = queue.shift();
+        if (cur[0].left) {
+            queue.push([cur[0].left, cur[1] + 1]);
+        }
+        if (cur[0].right) {
+            queue.push([cur[0].right, cur[1] + 1]);
+        }
+        if (queue[0] !== undefined && queue[0][1] === cur[1]) {
+            cur[0].next = queue[0][0];
+        } else {
+            cur[0].next = null;
+        }
+    }
+    return root;
+};
